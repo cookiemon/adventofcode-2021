ea82791bfa9fef83d8987910628a98996d94aa6a
Cookiemon <rhannek@gmx.de>
2021-12-03 15:37:34 +0100

    Solutions Day 03 Part 2

--- 03/solve.js
+++ 03/solve.js
@@ -3,6 +3,7 @@ const BITS = 12;
 const fs = require("fs");
 let input = fs
   .readFileSync("input", "utf8")
+  .trimEnd()
   .split("\n")
   .map((x) => x.split(""));
 
@@ -22,3 +23,34 @@ let binary_gamma = counts
 let gamma = parseInt(binary_gamma, 2);
 let epsilon = ~gamma & ((1 << BITS) - 1);
 console.log(`Part 1 solution: ${gamma * epsilon}`);
+
+// --- Part Two ---
+let o2 = input;
+let pos = 0;
+while (o2.length > 1) {
+  let zeros = o2.filter((x) => x[pos] == "0");
+  let ones = o2.filter((x) => x[pos] == "1");
+  if (zeros.length > o2.length / 2) {
+    o2 = zeros;
+  } else {
+    o2 = ones;
+  }
+  pos += 1;
+}
+o2 = parseInt(o2[0].join(""), 2);
+
+let co2 = input;
+pos = 0;
+while (co2.length > 1) {
+  let zeros = co2.filter((x) => x[pos] == "0");
+  let ones = co2.filter((x) => x[pos] == "1");
+  if (zeros.length <= co2.length / 2) {
+    co2 = zeros;
+  } else {
+    co2 = ones;
+  }
+  pos += 1;
+}
+co2 = parseInt(co2[0].join(""), 2);
+
+console.log(`Part 2 solution: ${o2 * co2}`);
